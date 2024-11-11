4ab52b1fc58031e6b98962282522bc6fc1f81b88
Sebastian Chaparro <sperezc5@upao.edu.pe>
2024-11-11 18:52:32 -0500

    add: model training and testing

--- app/src/main/java/com/example/learnme/activity/TrainingLoadActivity.kt
+++ app/src/main/java/com/example/learnme/activity/TrainingLoadActivity.kt
@@ -2,22 +2,80 @@ package com.example.learnme.activity
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
-import com.example.learnme.R
+import com.example.learnme.data.AppDatabase
+import com.example.learnme.data.ClassEntity
+import com.example.learnme.databinding.ActivityTrainingLoadBinding
+import com.example.learnme.fragment.TransferLearning
+import com.example.learnme.fragment.TransferLearningManager
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import org.tensorflow.lite.support.label.Category
+import java.util.Locale
 
-class TrainingLoadActivity : ComponentActivity() {
+class TrainingLoadActivity : ComponentActivity(), TransferLearning.ClassifierListener {
+
+    private lateinit var binding: ActivityTrainingLoadBinding
+    private lateinit var database: AppDatabase
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.activity_training_load)
+        binding = ActivityTrainingLoadBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val stopButton = findViewById<Button>(R.id.stopButton)
+        // Configurar la base de datos
+        database = AppDatabase.getInstance(this)
 
-        stopButton.setOnClickListener {
-            val intent = Intent(this, Step3Activity::class.java)
-            startActivity(intent)
+        // Iniciar configuración y entrenamiento de forma asincrónica
+        CoroutineScope(Dispatchers.Main).launch {
+            val numberOfClasses = getNumberOfClasses()
+            Log.d("TrainingLoadActivity", "Número de clases: ${numberOfClasses.size}")
+
+            // Inicializar TransferLearning en el Singleton
+            TransferLearningManager.initialize(
+                this@TrainingLoadActivity,
+                numberOfClasses,
+                this@TrainingLoadActivity
+            )
+
+            // Iniciar entrenamiento
+            TransferLearningManager.startTraining()
         }
 
+        binding.stopButton.setOnClickListener {
+            TransferLearningManager.pauseTraining()
+            startActivity(Intent(this, Step3Activity::class.java))
+        }
+    }
+
+
+    private suspend fun getNumberOfClasses(): List<ClassEntity> = withContext(Dispatchers.IO) {
+        // Obtener el número total de clases en la base de datos
+        database.classDao().getAllClasses().toList()
+    }
+
+    override fun onError(error: String) {
+        //Para el entrenamiento solo se muestra un error
+        runOnUiThread {
+            Toast.makeText(this, error, Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onResults(results: List<Category>?, inferenceTime: Long) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onLossResults(lossNumber: Float) {
+        String.format(
+            Locale.US,
+            "Loss: %.3f", lossNumber
+        ).let {
+            binding.tvLossConsumer.text = it
+        }
     }
 }
--- app/src/main/java/com/example/learnme/fragment/CameraHelper.kt
+++ app/src/main/java/com/example/learnme/fragment/CameraHelper.kt
@@ -1,25 +1,27 @@
 package com.example.learnme.fragment
 
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.camera.core.CameraSelector
-import androidx.camera.core.ImageCapture
-import androidx.camera.core.ImageCaptureException
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.ImageProxy
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.PreviewView
 import androidx.core.content.ContextCompat
-import java.io.File
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 
 class CameraHelper(
     private val activity: ComponentActivity,
     private val previewView: PreviewView,
-    private val onImageCaptured: (File) -> Unit
+    private val onImageCaptured: (ImageProxy) -> Unit,
 ) {
 
-    private lateinit var imageCapture: ImageCapture
+    private lateinit var imageAnalyzer: ImageAnalysis
     private val cameraExecutor: ExecutorService = Executors.newSingleThreadExecutor()
+    private var isInferenceMode: Boolean = false  // Estado de inferencia
+
 
     // Iniciar la cámara y configurar la vista previa
     fun startCamera() {
@@ -27,44 +29,69 @@ class CameraHelper(
         cameraProviderFuture.addListener({
             val cameraProvider = cameraProviderFuture.get()
 
-            val preview = Preview.Builder().build().also {
-                it.setSurfaceProvider(previewView.surfaceProvider)
-            }
-
-            imageCapture = ImageCapture.Builder().build()
             val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+            val preview =
+                Preview.Builder()
+                    .setTargetRotation(previewView.display.rotation)
+                    .build().also {
+                        it.setSurfaceProvider(previewView.surfaceProvider)
+                    }
+
+            // ImageAnalysis. Using RGBA 8888 to match how our models work
+            imageAnalyzer =
+                ImageAnalysis.Builder()
+                    .setTargetRotation(previewView.display.rotation)
+                    .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+                    .setOutputImageFormat(ImageAnalysis.OUTPUT_IMAGE_FORMAT_RGBA_8888)
+                    .build().also {
+                        if (isInferenceMode) {
+                            it.setAnalyzer(cameraExecutor) { image ->
+//                                Log.d("InferenceResult", "Inferencia iniciada y en progreso")
+                                onImageCaptured(image)  // Envía el ImageProxy a la actividad
+                                image.close()
+                            }
+                        }
+                    }
 
             try {
                 cameraProvider.unbindAll()
-                cameraProvider.bindToLifecycle(activity, cameraSelector, preview, imageCapture)
+                cameraProvider.bindToLifecycle(
+                    activity,
+                    cameraSelector,
+                    preview,
+                    imageAnalyzer
+                )
             } catch (exc: Exception) {
                 // Manejar error de la cámara
             }
         }, ContextCompat.getMainExecutor(activity))
     }
 
-    // Tomar una foto y guardar el archivo
+    // Función para tomar una foto en formato ImageProxy
     fun takePhoto() {
-        val imageFile = File(activity.externalMediaDirs.first(), "${System.currentTimeMillis()}.jpg")
-        val outputOptions = ImageCapture.OutputFileOptions.Builder(imageFile).build()
-
-        imageCapture.takePicture(
-            outputOptions,
-            ContextCompat.getMainExecutor(activity),
-            object : ImageCapture.OnImageSavedCallback {
-                override fun onError(exc: ImageCaptureException) {
-                    // Manejar error de captura
-                }
-
-                override fun onImageSaved(output: ImageCapture.OutputFileResults) {
-                    onImageCaptured(imageFile) // Llamada de retorno cuando se guarda la imagen
-                }
+        var captureMode = true
+        imageAnalyzer.setAnalyzer(cameraExecutor) { image ->
+            if (captureMode) {
+                onImageCaptured(image)  // Envía el ImageProxy a la actividad
+                captureMode = false
+                imageAnalyzer.clearAnalyzer()  // Detener el análisis después de capturar la imagen
+            } else {
+                image.close()
             }
-        )
+        }
     }
+
+    fun startInference() {
+        isInferenceMode = true
+        Log.d("InferenceResult", "Inferencia iniciada")
+    }
+
 
     // Liberar el ejecutor de la cámara
     fun shutdown() {
         cameraExecutor.shutdown()
+        isInferenceMode = false
+        imageAnalyzer.clearAnalyzer()
     }
 }
